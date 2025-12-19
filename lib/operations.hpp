dc0955049179981d3c6ed359304c14d407692f1a
Tristen Glover <tmg098@gmail.com>
2025-12-19 11:08:27 -0700

    Refactor operations into own files

---
+++ lib/operations.hpp
@@ -0,0 +1,10 @@
+#ifndef OPERATIONS_H
+
+#define OPERATIONS_H
+
+void add(int* x, int* y, int* result);
+void subtract(int* x, int* y, int* result);
+void multiply(int* x, int*y, int* result);
+void divide(int* x, int*y, int* result);
+
+#endif
